feea83f1133b636edfbadc3093e88b528adabdcb
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-02-23 16:31:48 -0800

    i have no idea

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string item;
   do
   {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
+
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 if (input == 'a' || input =='A')
@@ -23,15 +23,14 @@ if (input == 'a' || input =='A')
          {
          cout<<"You'll need a bigger list!";
          }
-         
-         
-   for (; numItems <5; numItems++)
-             {
-                cin>>list[numItems];
-             }
+               
+     cin>>item;
+     list[numItems]=item;
+     numItems++;
+               
     }
 
-}while ((input != 'q' && input != 'Q') || numItems <5 );
+}while ((input != 'q' && input != 'Q') && numItems <5 );
 
   return 0;
 }

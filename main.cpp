92e3ed4bf60e326b3c0d6eec74abfe771972bac8
Matthew S Scheer <mscheer1@mail.csuchico.edu>
2018-03-13 11:39:05 -0700

    changed it to vector for list engine

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector <string> list;
+ // string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
   string item;
   int outcount =0;
+  int newSize;
   
   do
   {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-    cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-if (input == 'a' || input == 'A')
+      cout<<"\n==GROCERY LIST MANAGER==";
+      cout<<"\nEnter your choice: ";
+      cout<<"\n (A)dd an item";
+      cout<<"\n (Q)uit";
+      cout<<"\nYour choice (A/Q): ";
+      cin>>input;
+  if (input == 'a' || input == 'A')
     {
           cout<<"What is the item?"<<endl;
           cin>>item;
-     if (numItems > 4)
-         {
-              cout<<"You'll need a bigger list!" <<endl;
-              
-         }else
-         {
-              
-             list[numItems]=item;
+          list.push_back(item);
+          numItems++;
 
-         }
-         
-     numItems++;
     }
    
-}while ((input != 'q' && input != 'Q') && numItems < 8);
+  }while ((input != 'q' && input != 'Q'));
 cout <<"==ITEMS TO BUY==" << endl;
-while ( outcount < 5) 
-{
-    cout << outcount + 1 << " " << list[outcount] << endl;
-    outcount++;
- }
+do 
+{   if (list.empty())
+    {
+        cout << "No items to buy!" << endl;
+    }else
+    {
+         cout << outcount + 1 << " " << list[outcount] << endl;
+         outcount++;
+    }
+ }while (outcount < numItems );
 
   return 0;
 }
+
